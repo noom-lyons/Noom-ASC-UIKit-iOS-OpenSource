dc049ae685d919b0e73379f65844326f55e5a9dd
Michael Lyons <michael.lyons@noom.com>
2024-09-17 09:59:05 -0400

    add resource xib

--- Package.swift
+++ Package.swift
@@ -39,7 +39,10 @@ let package = Package(
                 "RealmSwift",
                 "AmitySDK"
             ],
-            path: "UpstraUIKit/UpstraUIKit"
+            path: "UpstraUIKit/UpstraUIKit",
+            resources: [
+                .process("Components/AmityBadgeView.xib")
+            ]
         )
     ]
 )
